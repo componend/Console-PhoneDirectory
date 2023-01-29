479017a0bd1164e97a9a1ee4a65d92fdc47b1734
componend <96773794+componend@users.noreply.github.com>
2023-01-29 17:46:22 +0300

    Add files via upload

---
+++ 1-telefon_rehberi/telefon_rehberi/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace telefon_rehberi
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Menu();
+        }
+        public static void Menu()
+        {
+
+            Console.Clear();
+            var run = new run();
+            Console.WriteLine("1. KİŞİ EKLE\n2. KİŞİLERİ LİSTELE\n3. ADA GÖRE ARA\n4. SİL\n5. ÇIKIŞ");
+            string secim = Console.ReadLine();
+            if (secim == "1")
+            {
+                run.kisiEkle();
+                Menu();
+            }
+            if (secim == "2")
+            {
+                run.kisileriListele();
+                Menu();
+            }
+            if (secim == "3")
+            {
+                Console.Write("Aranacak ismi giirniz : ");
+                string metin = Console.ReadLine();
+                Kisiler gelenKisi = run.ara(metin);
+                if (gelenKisi!=null)
+                {
+                    Console.WriteLine($"Adı ve Soyadı : {gelenKisi.adSoyad}");
+                    Console.WriteLine("------------------------");
+                    for (int j = 0; j < gelenKisi.telefon.Length; j++)
+                    {
+                        Console.WriteLine("Telefon " + (j + 1) + $"     : {gelenKisi.telefon[j]}");
+                    }
+                    Console.WriteLine("------------------------");
+
+                }
+                else { Console.WriteLine($"{metin} isminde bir kayıt bulunamadı!!"); }
+                Console.WriteLine("Devam etmek için bir tuşa basın");
+                Console.ReadLine();
+                Menu();
+            }
+            if(secim=="4")
+            {
+                Console.Write("Aranacak ismi giirniz : ");
+                string metin = Console.ReadLine();
+                run.sil(metin);
+                Menu();
+            }
+        }
+
+    }
+
+    public class Kisiler
+    {
+        public string adSoyad;
+        public string[] telefon;
+
+    }
+
+}
---
+++ 1-telefon_rehberi/telefon_rehberi/run.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace telefon_rehberi
+{
+
+
+    public class run
+    {
+        
+         static Kisiler[] Kisi = new Kisiler[0];
+         
+        public void kisiEkle()
+        {
+            Console.Clear();
+            Kisiler k = new Kisiler();
+            Console.Write("Adı ve Soyadı : ");
+            k.adSoyad = Console.ReadLine();
+          
+                Console.Write("kac telfon numarası girilecek : ");
+                int telAdet =Convert.ToInt32( Console.ReadLine());
+                Array.Resize(ref k.telefon, telAdet);
+                for (int i = 0; i < k.telefon.Length; i++)
+                {
+                    Console.Write("Telefonu(" + (i+1) + ") : ");
+                    k.telefon[i] = Console.ReadLine();
+                }
+            Array.Resize(ref Kisi, Kisi.Length + 1);
+            Kisi[Kisi.Length - 1] = k;
+            Console.WriteLine("1 kayıt eklendi");
+            Console.WriteLine("Devam etmek için bir tuşa basın");
+            Console.ReadLine();
+
+        }
+        public void kisileriListele()
+        {
+            Console.Clear();
+            for (int i = 0; i < Kisi.Length; i++)
+            {
+                Console.WriteLine($"Adı ve Soyadı : {Kisi[i].adSoyad}");
+                for (int j = 0; j <Kisi[i].telefon.Length; j++)
+                {
+                    Console.WriteLine("Telefon "+(j+1)+ $"     : {Kisi[i].telefon[j]}");
+                }
+               
+                Console.WriteLine("------------------------");
+            }
+            Console.WriteLine("Devam etmek için bir tuşa basın");
+            Console.ReadLine();
+        }
+
+        public Kisiler ara(string ad)
+        {
+            Console.Clear();
+            for (int i = 0; i < Kisi.Length; i++)
+            {
+                if (Kisi[i].adSoyad == ad)
+                {
+                    return Kisi[i];
+                }
+            }
+            return null; ;
+        }
+        public void sil(string ad)
+        {
+            int varYok = 0;
+            for (int i = 0; i < Kisi.Length; i++)
+            {
+                if (Kisi[i].adSoyad == ad)
+                {
+                    if ((i + 1) < Kisi.Length)
+                    {
+                        for (int j = i; j < Kisi.Length-1; j++)
+                        {
+                            Kisi[j] = Kisi[j + 1];
+                           
+                        }
+
+                        Kisi[Kisi.Length - 2] = Kisi[Kisi.Length - 1];
+                        Kisi[Kisi.Length - 1] = null;
+                        Array.Resize(ref Kisi, Kisi.Length - 1);
+                        varYok = 1;
+                        Console.WriteLine($"{ad} isnindeki kayıt silindi!!");
+                        Console.WriteLine("Devam etmek için bir tuşa basın");
+                        Console.ReadLine();
+                        break;
+                    }
+                    else {
+                        Kisi[Kisi.Length - 1] = null;
+                        Array.Resize(ref Kisi,Kisi.Length-1);
+                        varYok = 1;
+                        Console.WriteLine($"{ad} isnindeki kayıt silindi!!");
+                        Console.WriteLine("Devam etmek için bir tuşa basın");
+                        Console.ReadLine();
+                        break;
+                    }
+                    
+                }
+
+            }
+
+            if (varYok==0) { 
+                Console.WriteLine($"{ad} isminde bir kayıt bulunamadı!!");
+                Console.WriteLine("Devam etmek için bir tuşa basın");
+                Console.ReadLine();
+          }
+        }
+
+
+    }
+}
